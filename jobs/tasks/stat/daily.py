0f0664c33e6c03ec19aee5c86f64c800ab0ccc39
master <631871612@qq.com>
2019-04-02 20:57:02 +0800

    商品售卖统计任务

--- jobs/tasks/stat/daily.py
+++ jobs/tasks/stat/daily.py
@@ -7,10 +7,11 @@ from common.models.stat.StatDailyFood import StatDailyFood
 from common.models.stat.StatDailyMember import StatDailyMember
 from common.models.stat.StatDailySite import StatDailySite
 from common.models.food.WxShareHistory import WxShareHistory
+from common.models.food.FoodSaleChangeLog import FoodSaleChangeLog
 from sqlalchemy import func
 import random
 '''
-python manager.py runjob -m stat/daily -a member \ food \ site -p 2019-04-06      # 不传默认是当天
+python manager.py runjob -m stat/daily -a member | food | site -p 2019-04-06      # 不传默认是当天
 '''
 
 
@@ -20,8 +21,8 @@ class JobTask():
 
 	def run(self, params):
 		act = params['act'] if 'act' in params else ''
-		date = params['param'][0] if params['param'] and len(params['param']) > 1 else getFormatDate(format="%Y-%m-%d")
-		app.logger.info(act)
+		date = params['param'][0] if params['param'] and len(params['param']) >= 1 else getFormatDate(format="%Y-%m-%d")
+		app.logger.info(date)
 		if not act:
 			return
 		date_from = date + " 00-00-00"
@@ -37,13 +38,13 @@ class JobTask():
 		if act == 'member':
 			self.statMember(func_params)
 		elif act == 'food':
-			pass
+			self.statFood(func_params)
 		elif act == 'site':
 			pass
 
-		'''
-		会员统计
-		'''
+	'''
+	会员统计
+	'''
 	def statMember(self, params):
 		date = params['date']
 		date_from = params['date_from']
@@ -88,6 +89,48 @@ class JobTask():
 			tmp_model_stat_member.updated_time = getCurrentDate()
 			db.session.add(tmp_model_stat_member)
 			db.session.commit()
+		return
 
+	'''
+	商品售卖统计
+	'''
+	def statFood(self, params):
+		date = params['date']
+		date_from = params['date_from']
+		date_to = params['date_to']
+		act = params['act']
+		app.logger.info("act:{0}, from:{1}, to:{2}".format(act, date_from, date_to))
+
+		stat_food_list = db.session\
+			.query(FoodSaleChangeLog.food_id, func.sum(FoodSaleChangeLog.quantity).label('total_count'), func.sum(FoodSaleChangeLog.price).label('total_pay_money'))\
+			.filter(FoodSaleChangeLog.created_time >= date_from, FoodSaleChangeLog.created_time <= date_to)\
+			.group_by(FoodSaleChangeLog.food_id)\
+			.all()
+
+		if not stat_food_list:
+			app.logger.info("no food list")
+			return
+
+		for item in stat_food_list:
+			tmp_food_id = item[0]
+			tmp_stat_food = StatDailyFood.query.filter_by(date=date, food_id=tmp_food_id).first()
+			if tmp_stat_food:
+				tmp_model_stat_food = tmp_stat_food
+			else:
+				tmp_model_stat_food = StatDailyFood()
+				tmp_model_stat_food.food_id = tmp_food_id
+				tmp_model_stat_food.date = date
+				tmp_model_stat_food.created_time = getCurrentDate()
+
+			tmp_model_stat_food.total_count = item[1]
+			tmp_model_stat_food.total_pay_money = item[2]
+			'''
+			为了测试效果模拟数据
+			'''
+			# tmp_model_stat_food.total_shared_count = random.randint(50, 100)
+			# tmp_model_stat_food.total_pay_money = random.randint(1000, 1010)
+			tmp_model_stat_food.updated_time = getCurrentDate()
+			db.session.add(tmp_model_stat_food)
+			db.session.commit()
 		return
 
